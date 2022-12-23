68bbdafa37d392033553417952eb34690d52bd92
ericwalkerw <ericwalker.leo@gmail.com>
2022-12-23 14:20:39 +0700

    23/12/2022

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,16 +9,13 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float attackCooldown;  
-    [SerializeField] private float reloadTime;
-    [SerializeField] private int maxAmmo;
 
     [SerializeField] private GameObject butlletPrefab;
     [SerializeField] private GameObject BloodPre;
     [SerializeField] private GameObject fireBurst;
+
     [SerializeField] private Transform gunPoint;
-    [SerializeField] private TMP_Text amountBullet;
     [SerializeField] private AudioClip shootSound;
-    [SerializeField] private AudioClip reloadSound;
 
 
     private Animator anim;
@@ -28,55 +25,26 @@ public class Player : MonoBehaviour
 
     private float cooldownTimer = Mathf.Infinity;
 
-    private int currentAmmo;
-
-    private bool isReadloaing = false;
-
-    // Start is called before the first frame update
     private void Start()
     {
-        if (currentAmmo == -1)
-            currentAmmo = maxAmmo;
         player = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        amountBullet.text = currentAmmo + "/" + maxAmmo;
-        if (isReadloaing)
-        {
-            return;
-        }
-
         cooldownTimer += Time.deltaTime;
 
         input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         player.velocity = input.normalized * speed;
 
         transform.right = (MousePos.GetMouse() - (Vector2)transform.position).normalized;
-        if (currentAmmo <= 0 || Input.GetKeyDown(KeyCode.R))
-        {
-            StartCoroutine(Reloading());
-            return;
-        }
-        if (Input.GetButtonDown("Fire1") && cooldownTimer > attackCooldown)
+
+        if (Input.GetButtonDown("Fire1") && cooldownTimer > attackCooldown )
         {
             Shoot();      
         }
         SetAnim();       
-    }
-
-    IEnumerator Reloading()
-    {
-        isReadloaing = true;
-        SoundManeger.instance.PlaySound(reloadSound);
-        anim.SetTrigger("reload");
-        yield return new WaitForSeconds(reloadTime);
-     
-        currentAmmo = maxAmmo;
-        isReadloaing = false;
     }
 
     private void SetAnim()
@@ -91,9 +59,7 @@ public class Player : MonoBehaviour
         anim.SetTrigger("shoot");
         Instantiate(butlletPrefab, gunPoint.position, transform.rotation);
         Instantiate(fireBurst, gunPoint.position, transform.rotation);
-        currentAmmo--;
     }
-
     public void Die()
     {    
         Instantiate(BloodPre, transform.position, Quaternion.identity);       
--- Assets/Scripts/UI/uiMenu.cs
+++ Assets/Scripts/UI/uiMenu.cs
@@ -8,53 +8,11 @@ using UnityEditor;
 
 public class uiMenu : MonoBehaviour
 {
-    public AudioMixer audioMixer;
-
-    public TMP_Dropdown resolutionDropdown;
-
     public Slider musicSlider;
-
-    Resolution[] resolutions;
 
     public void MusicVolume()
     {
         AudioManeger.instance.MusicVolume(musicSlider.value);
-    }
-
-    public void Start()
-    {
-        resolutions = Screen.resolutions;
-
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-
-        int currenResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) 
-            {
-                currenResolutionIndex = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currenResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-    }
-
-
-    public void Sound(float Sound)
-    {
-        audioMixer.SetFloat("Sound", Sound);
-    }
-    public void SetResolution(int resolutionIndex)
-    {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width,resolution.height, Screen.fullScreen);
     }
 
     public void SetQuality(int qualitiIndex)
